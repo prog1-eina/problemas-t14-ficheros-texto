84e595bc9d1bdd1b960328111f6e4c6ba75d58ad
Miguel Ángel Latre Abadía <latre@unizar.es>
2022-11-25 17:39:03 +0100

    Cambiado espacio de no separación por espacio normal

--- 6-frecuencias.cpp
+++ 6-frecuencias.cpp
@@ -3,15 +3,15 @@
  * Autores: Javier Martínez y Miguel Ángel Latre
  * Última revisión: 29 de noviembre de 2021
  * Resumen: Soluciones a los problemas del tema 14: frecuencias de letras
- * Nota: El programa completo está contenido en este fichero, por lo que puede 
- *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
- *       Code.
- *       También puede compilarse desde la terminal través de la orden
- *           g++ -Wall -Wextra 6-frecuencias.cpp -o 6-frecuencias
- *       y ejecutarse en Windows a través de la orden
- *           .\6-frecuencias.exe
- *       o en Linux y macOS
- *           ./6-frecuencias
+ * Nota: El programa completo está contenido en este fichero, por lo que puede 
+ *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
+ *       Code.
+ *       También puede compilarse desde la terminal través de la orden
+ *           g++ -Wall -Wextra 6-frecuencias.cpp -o 6-frecuencias
+ *       y ejecutarse en Windows a través de la orden
+ *           .\6-frecuencias.exe
+ *       o en Linux y macOS
+ *           ./6-frecuencias
 \******************************************************************************/
 
 #include <iostream>
